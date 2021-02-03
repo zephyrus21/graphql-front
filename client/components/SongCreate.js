9cc955db28fb6217ac4d5ee53661d410d511ed80
zephyrus21 <pandey.piyus@gmail.com>
2021-02-03 16:26:41 +0530

    adding mutattion

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
 
 export class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    //! to invoke the mutate in graphql mutation
+    this.props
+      .mutate({
+        variables: { title: this.state.title },
+      })
+      .then(() => hashHistory.push('/'));
+  }
+
+  onChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a new Song</h3>
-        <form action=""></form>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.onChange.bind(this)}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
@@ -14,7 +15,15 @@ class SongList extends Component {
   render() {
     console.log(this.props.data.songs);
     if (this.props.data.loading) return <div>Loading...</div>;
-    else return <ul className="collection">{this.renderSongs()}</ul>;
+    else
+      return (
+        <div>
+          <ul className="collection">{this.renderSongs()}</ul>
+          <Link to="/songs/new" className="btn-floating btn-large red right">
+            <i className="material-icons">add</i>
+          </Link>
+        </div>
+      );
   }
 }
 
