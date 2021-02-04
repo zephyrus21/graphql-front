1a663a8bb47df930b4aabd57a57864afbde1d180
zephyrus21 <pandey.piyus@gmail.com>
2021-02-04 13:29:29 +0530

    adding lyric create

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link, hashHistory } from 'react-router';
-
 import { graphql } from 'react-apollo';
 
+import LyricCreate from './LyricCreate';
 import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
@@ -18,6 +18,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
